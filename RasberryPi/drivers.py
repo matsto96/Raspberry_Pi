43aebdabe9b46108aafd6eb2eb1e16338b27572d
Jostein Brovold <jostein.brovold@revolve.no>
2020-02-06 15:37:55 +0100

    Added butterworth filter drivers

--- RasberryPi/drivers.py
+++ RasberryPi/drivers.py
@@ -1,6 +1,19 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
 def CCR(data, ADC_CH1, ADC_CH2):
     for i in range(data.shape[1]):
